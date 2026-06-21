f2a4e69d55d54fae1ba5bc4e27227200488bde2e
Hikaru Hotta <46637503+hikaruhotta@users.noreply.github.com>
2026-06-20 23:14:19 -0700

    Add: hand-of-straights - submission-4

---
+++ Data Structures & Algorithms/hand-of-straights/submission-4.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+
+        cnt = Counter(hand)
+        while cnt:
+            min_val = min(list(cnt.keys()))
+            for i in range(groupSize):
+                print(min_val + i in cnt, min_val, cnt)
+                if min_val + i not in cnt:
+                    return False
+                else:
+                    cnt[min_val + i] -= 1
+                    if cnt[min_val + i] == 0:
+                        del cnt[min_val + i]
+        return True
+
+
+
