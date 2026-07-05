f008a2d364ac2222915a118be3bdd814b663fb7d
Hikaru Hotta <46637503+hikaruhotta@users.noreply.github.com>
2026-07-04 22:32:50 -0700

    Add: car-pooling - submission-1

---
+++ Data Structures & Algorithms/car-pooling/submission-1.py
@@ -0,0 +1,28 @@
+import heapq
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        scheduled_min_heap = []
+        for trip in trips:
+            num_passengers_i, from_i, to_i = trip[0], trip[1], trip[2]
+            heapq.heappush(scheduled_min_heap, (from_i, to_i, num_passengers_i))
+        
+        active_min_heap = []
+
+        num_passengers = 0
+
+        while scheduled_min_heap:
+            (from_i, to_i, num_passengers_i) = heapq.heappop(scheduled_min_heap)
+
+            while active_min_heap and active_min_heap[0][0] <= from_i:
+                to_j, num_passengers_j = heapq.heappop(active_min_heap)
+                num_passengers -= num_passengers_j
+            
+            if num_passengers + num_passengers_i > capacity:
+                return False
+            
+            num_passengers += num_passengers_i
+            heapq.heappush(active_min_heap, (to_i, num_passengers_i))
+        
+        return True
+            
