3c7b0d5120b32354e0c2e0dde7896dcb00336f7a
Hikaru Hotta <46637503+hikaruhotta@users.noreply.github.com>
2026-07-06 22:43:33 -0700

    Add: accounts-merge - submission-5

---
+++ Data Structures & Algorithms/accounts-merge/submission-5.py
@@ -0,0 +1,58 @@
+from collections import deque
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        graph = {}
+        for i, account in enumerate(accounts):
+            name = account[0]
+            emails = account[1:]
+            for email in emails:
+                if email not in graph:
+                    graph[email] = []
+                graph[email].append(i)
+
+        print(graph)
+
+        merge_graph = {i: set() for i in range(len(accounts))}
+        for key, values in graph.items():
+            if len(values) > 0:
+                for i in values:
+                    for j in values:
+                        if i != j:
+                            merge_graph[i].add(j)
+
+        visited = set()
+        groups = []
+        for key in merge_graph:
+            if key not in visited:
+                groups.append(self.helper(key, merge_graph, visited))
+
+        result = []
+        for group in groups:
+            emails = set()
+            for i in group:
+                for email in accounts[i][1:]:
+                    emails.add(email)
+            emails = sorted(list(emails))
+            result.append([accounts[group[0]][0]] + emails)
+
+        return result
+
+    def helper(self, account: int, graph: Dict, visited: set()) -> List[int]:
+        result = []
+        queue = deque([account])
+        visited.add(account)
+        while queue:
+            acct = queue.popleft()
+            result.append(acct)
+
+            for node in graph[acct]:
+                if node not in visited:
+                    visited.add(node)
+                    queue.append(node)
+        return result
+
+
+        
+
+        
